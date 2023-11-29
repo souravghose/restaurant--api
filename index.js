6f1bd09ac6f707f23dd87be6ca8a11391896a049
Sourav Ghose <66877167+souravghose@users.noreply.github.com>
2023-11-30 00:24:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 3000; // Use environment port or 3000 as defaul
 const dataPath = './data.json';
 const rawData = fs.readFileSync(dataPath);
 const restaurants = JSON.parse(rawData);
+// Route to get a specific menu item by ID
+app.get('/menu/:id', (req, res) => {
+  const menuId = parseInt(req.params.id);
+  const menuItem = menuData.find(item => item.id === menuId);
+
+  if (menuItem) {
+    res.json(menuItem);
+  } else {
+    res.status(404).json({ message: 'Menu item not found' });
+  }
+});
 
 // Endpoint to get all restaurants and their menus
 app.get('/restaurants', (req, res) => {
